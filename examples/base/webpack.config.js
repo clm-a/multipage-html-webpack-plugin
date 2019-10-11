8881aa1030decb46ff588d03b87f5eb4af4af03b
Clément Alexandre <clmntlxndr@gmail.com>
2019-10-11 13:44:39 +0200

    Add base example

---
+++ examples/base/webpack.config.js
@@ -0,0 +1,28 @@
+// From a Webpack default configuration, only highlighting what matters here
+
+const MultipageHtmlWebpackPlugin = require('multipage-html-webpack-plugin')
+
+module.exports = {
+
+	entry: './src/index.js', 						// All entries will be added to pages
+																			// as it's done by HtmlWebpackPlugin
+																			// by default
+	// .....
+	plugins: [
+    // .....
+    new MultipageHtmlWebpackPlugin(), // * Defaults serves src/index.html within
+                                      //   src/layout.html
+    new MultipageHtmlWebpackPlugin({
+      pages: './src/pages/*.html',		// * How to find pages on the local filesystem
+      pagesPath: 'pages/',            // * Page partials directory, relative to the
+      																//   layout, used both for partial inclusion
+  		  															//   and document url location
+			htmlWebpackPluginOptions: {     // * Pass any option to HtmlWebpackPlugin
+				meta: {
+					viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+				}
+			}
+		})
+  ],
+  // .....
+};
